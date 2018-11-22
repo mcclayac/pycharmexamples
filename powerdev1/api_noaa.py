bfe8125eb80a6367a4f6d677c19cb98a7416f2a5
mcclayac <anthony.mcclay@me.com>
2018-11-22 04:35:06 -0600

    SQL-Lite examples (incomplete)  API foor NOAA

---
+++ powerdev1/api_noaa.py
@@ -0,0 +1,25 @@
+"""
+        
+   created: mcclayac
+   Company Name : BigMAN Software
+   MyName: Tony McClay
+   date: 11/21/18
+   day of month: 21
+   
+   Project Name: pycharmexamples
+   filename:  
+   package name: 
+   IDE: PyCharm
+   
+   
+"""
+import json
+import requests
+
+url_template = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/'  //{endpoint}
+end_point = '/locations'
+
+my_token = ""
+
+
+
---
+++ powerdev1/sql-lite.py
@@ -0,0 +1,20 @@
+"""
+        
+   created: mcclayac
+   Company Name : BigMAN Software
+   MyName: Tony McClay
+   date: 11/21/18
+   day of month: 21
+   
+   Project Name: pycharmexamples
+   filename:  
+   package name: 
+   IDE: PyCharm
+   
+   
+"""
+
+import sqlite3
+
+conn = sqlite3.connect('exampleSQLLite.db')
+
